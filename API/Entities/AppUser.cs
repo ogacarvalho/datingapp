5de2fb0da9887e14da24972e7b72fb1ed06ac3e2
GC <gabriel.carvalho@avitaseg.com.br>
2023-01-23 07:21:34 -0300

    Update

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -4,5 +4,9 @@ namespace API.Entities
     {
         public int Id { get; set; } // Ao usar o nome "Id" automaticamente o migration o criará como primary key. Se não quiser, precisará adicionar um "DataAnnotation" que incluirá um [Key] em cima da coluna cujo nome não é "Id".
         public string UserName { get; set; } //Pascal Casing its a C# pattern.
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
     }
 }
