1825d19dd863aebc4577939144aeaf2229eb0c11
GC <gabriel.carvalho@avitaseg.com.br>
2023-01-23 06:57:49 -0300

    Update

--- API/Program.cs
+++ API/Program.cs
@@ -13,9 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(); // Estamos habilitando o serviço de Cors (Cros Origins Resorces)
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200")); // Habilitando que o localhost específico, (front) pode consumir a api.
+
 app.MapControllers(); // Middleware que dirá aonde esta o endpoint procurado.
 
 app.Run();
